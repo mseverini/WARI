21705ee816087c2d53764d12cd96c1ad7354ec58
Matt Severini <mavenlink@Matt-Severinis-MacBook-Pro.local>
2018-03-06 19:23:51 -0700

    add welcome text to sign in page

--- frontend/src/components/Authentication/LogInForm.jsx
+++ frontend/src/components/Authentication/LogInForm.jsx
@@ -7,16 +7,20 @@ import {Link} from 'react-router-dom'
 
 const LogInForm = ({handleChange, handleSubmit, ...props}) => {
   return (
-    <Grid fluid={true}>
-      <Form onSubmit={handleSubmit}>
-        <FieldGroup {...props} id={'login-field-email'} name={'email'} label={'Email'} type={'email'} onChange={handleChange}/>
-        <FieldGroup {...props} id={'login-field-password'} type={'password'} name={'password'} label={'Password'} onChange={handleChange}/>
-        <span className='center'>
-          <Button type={'submit'}>Log In</Button>
-          <Link to={'/SignUp'} style={{marginLeft:'15px'}}><Button>Sign Up</Button></Link>
-        </span>
-      </Form>
-    </Grid>
+    <div>
+      <h2 className='center'>Welcome to the SLCA's bolt database</h2>
+      <h4 className='center'> We're stoked you're here </h4>
+      <Grid fluid={true}>
+        <Form onSubmit={handleSubmit}>
+          <FieldGroup {...props} id={'login-field-email'} name={'email'} label={'Email'} type={'email'} onChange={handleChange}/>
+          <FieldGroup {...props} id={'login-field-password'} type={'password'} name={'password'} label={'Password'} onChange={handleChange}/>
+          <span className='center'>
+            <Button type={'submit'}>Log In</Button>
+            <Link to={'/SignUp'} style={{marginLeft:'15px'}}><Button>Sign Up</Button></Link>
+          </span>
+        </Form>
+      </Grid>
+    </div>
   )
 }
 
