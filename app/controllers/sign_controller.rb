3c1ae109037994e3931b336ff4bf5585ff76967a
Matt Severini <mavenlink@Matt-Severinis-MacBook-Pro.local>
2018-03-16 10:08:50 -0600

    protect signing controller

--- app/controllers/sign_controller.rb
+++ app/controllers/sign_controller.rb
@@ -1,4 +1,6 @@
 class SignController < ApplicationController
+  before_action :cors_set_access_control_header
+
   def sign
 
     s3 = Aws::S3::Resource.new(region:'us-east-2')
@@ -10,5 +12,13 @@ class SignController < ApplicationController
     respond_to do |format|
       format.json { render json: {signedUrl: url} }
     end
+  end
+
+  private
+  def cors_set_access_control_header
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 end
