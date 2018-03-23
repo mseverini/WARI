1ad25c5dcec115ca6bcf7db4f1ea04addea95eb1
Matt Severini <mavenlink@Matt-Severinis-MacBook-Pro.local>
2018-03-23 16:49:44 -0600

    make the ratings route the default route

--- app/controllers/bolt_ratings_controller.rb
+++ app/controllers/bolt_ratings_controller.rb
@@ -1,14 +1,8 @@
 class BoltRatingsController < ApplicationController
-  before_action :set_climbing_route, only: [:show, :edit, :update, :destroy]
-
-  # GET /climbing_routes
-  # GET /climbing_routes.json
   def index
     @bolt_ratings = BoltRating.all.order(:rating)
   end
 
-  # GET /climbing_routes/1
-  # GET /climbing_routes/1.json
   def show
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,6 @@ Rails.application.routes.draw do
   resources :bolt_ratings, :only => [:index]
   resources :areas
 
-  root 'areas#index'
+  root 'bolt_ratings#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
